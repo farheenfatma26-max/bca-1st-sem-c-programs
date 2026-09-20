603c6c9cef2001bda5084e44554aed919dc3c26c
Farheen Fatma <farheen.fatma26@vupune.ac.in>
2026-09-20 11:55:17 +0530

    Create Type:12_speed_distance_time.c

---
+++ Type:12_speed_distance_time.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float speed, time, distance;
+    printf("Enter speed and time: ");
+    scanf("%f %f", &speed, &time);
+    distance = speed * time;
+    printf("Distance = %.2f", distance);
+    return 0;
+}
